b02f0a419cea4f8bdabc8dccfd3150a5c6e632be
Junqi Wu <juwu@odoo.com>
2025-07-14 13:59:29 -0400

    [IMP] odoo_lunch: sync checkin with calendar event

--- odoo_lunch/controllers/main.py
+++ odoo_lunch/controllers/main.py
@@ -4,24 +4,23 @@ from odoo.http import route, request
 import qrcode
 import io
 import base64
-from datetime import date
+from datetime import date, datetime
 
 class LunchKioskController(http.Controller):
     @route('/lunch_kiosk', type='http', auth='public')
     def lunch_kiosk(self):
-        # Get current date in yyyy-mm-dd format
-        current_date_str = date.today().strftime('%Y-%m-%d')
-
-        # Build target URL
-        target_url = f"{request.httprequest.host_url}lunch_kiosk/check_in/{current_date_str}"
+        today = date.today()
+        lunch_menu = request.env['lunch.menu'].sudo().search([
+            ('date', '>=', datetime.combine(today, datetime.min.time())),
+            ('date', '<=', datetime.combine(today, datetime.max.time())),
+        ], limit=1)
+        target_url = f"{request.httprequest.host_url}lunch_kiosk/check_in/{lunch_menu.id}"
         
-        # Generate QR code that points to "/test"
         qr = qrcode.QRCode(box_size=10, border=4)
         qr.add_data(target_url)
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
 
-        # Convert QR image to base64
         buffer = io.BytesIO()
         img.save(buffer, format="PNG")
         img_str = base64.b64encode(buffer.getvalue()).decode()
@@ -30,13 +29,24 @@ class LunchKioskController(http.Controller):
             'qr_code': img_str,
         })
 
-    @route('/lunch_kiosk/check_in/<string:check_in_date>', type='http', auth='user')
-    def lunch_check_in(self, check_in_date, **kwargs):
+    @route('/lunch_kiosk/check_in/<int:menu_id>', type='http', auth='user')
+    def lunch_check_in(self, menu_id, **kwargs):
         username = request.env.user.name
+
+        user_partner = request.env.user.partner_id
+
+        menu = request.env['lunch.menu'].sudo().browse(menu_id)
+        event = menu.event
+        attendee = event.attendee_ids.filtered(lambda a: a.partner_id == user_partner)
+        if not attendee or attendee.state != 'accepted':
+            return request.render('odoo_lunch.lunch_checkin_error', {
+                'username': username,
+            })
 
         # TODO set the user as checked-in in the backend
 
         return request.render('odoo_lunch.lunch_checked_in', {
             'username': username,
-            'check_in_date': check_in_date,
+            'menu_name': menu.name,
+            'allergy_info': menu.allergy_info,
         })
