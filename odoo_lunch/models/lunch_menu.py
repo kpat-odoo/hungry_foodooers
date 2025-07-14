1e88e2e3d57d9a723fd9d5e536d4b79770e0518c
Ngoc Anh Thy Ly (natl) <natl@odoo.com>
2025-07-14 13:58:44 -0400

    add calendar view changes

--- odoo_lunch/models/lunch_menu.py
+++ odoo_lunch/models/lunch_menu.py
@@ -1,7 +1,12 @@
 from odoo import fields, models, api
+<<<<<<< Updated upstream
 
 from datetime import datetime, time
 
+=======
+from dateutil.relativedelta import relativedelta
+ 
+>>>>>>> Stashed changes
 
 class LunchMenu(models.Model):
     _name = "lunch.menu"
@@ -30,6 +35,7 @@ class LunchMenu(models.Model):
         ],
         default="main_course",
     )
+<<<<<<< Updated upstream
     event = fields.Many2one('calendar.event', string='Calendar Event')
 
 
@@ -74,3 +80,12 @@ class LunchMenu(models.Model):
             event = self.env['calendar.event'].create(event_vals)
             self.event = event.id
 
+=======
+    is_all_week = fields.Boolean(string="For All Week", default=False)
+    date_end = fields.Date(string="End Date", compute="_compute_end_date")
+
+    @api.depends("is_all_week")
+    def _compute_end_date(self):
+        for menu in self:
+            menu.date_end = menu.date + relativedelta(days=(4 - menu.date.weekday())) if menu.is_all_week else menu.date
+>>>>>>> Stashed changes
